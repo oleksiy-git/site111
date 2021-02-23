c7305c8197abb8f34ee42a6721e1e005a0929d70
oleksiy-git <oleksiy.git@gmail.com>
2021-02-23 00:48:28 +0000

    resume pdf upd

--- app/main.js
+++ app/main.js
@@ -23,6 +23,21 @@ function setSelectedFileName(fileName) {
     "." +
     fileName.getElementsByTagName("span").item(1).innerHTML; // file extension
 }
+
+// this function creates Web element to show an "Error" message when user tries to navigate to the "Root" folder
+function root() {
+  createBlankPage();
+
+  var errorPageContainer = document.createElement("div");
+  var errorPageContent = document.createElement("div");
+  errorPageContainer.id = "alert-page-container";
+  errorPageContent.id = "alert-page-content";
+  document.getElementById("blank-page").appendChild(errorPageContainer);
+  document.getElementById("alert-page-container").appendChild(errorPageContent);
+  errorPageContent.innerHTML =
+    '<div class="top-path-label red-label">Access Denied</div><div class="popup-content"><p>You don&#39;t have an access to the "Root" folder</p></div><div><br><span class="delete-yes-no-button" onclick="closeHelpPage()">[ Close this window ]</span><br><br></div>';
+}
+
 // this function shows "About" information on the right panel
 function aboutInfo() {
   removeActiveLineStyle();
@@ -43,6 +58,40 @@ function blogInfo() {
   activeLine.classList.add("active-line");
   var rightContent = document.getElementById("right-content");
   rightContent.innerHTML = '<p>Some "Blog" text to be added here..</p>';
+
+  setSelectedFileName(activeLine); // shows file name on the right panel - bottom file name section
+}
+
+// this function creates Web element to show an "Info" message when user tries to run a contact.exe programme
+function contactExe() {
+  createBlankPage();
+  removeActiveLineStyle();
+
+  var activeLine = document.getElementById("contact-exe");
+  activeLine.classList.add("active-line");
+  var rightContent = document.getElementById("right-content");
+  rightContent.innerHTML =
+    "<p>Preview is not available for executable programme.</p>";
+  var infoPageContainer = document.createElement("div");
+  var infoPageContent = document.createElement("div");
+  infoPageContainer.id = "quit-page-container";
+  infoPageContent.id = "quit-page-content";
+  document.getElementById("blank-page").appendChild(infoPageContainer);
+  document.getElementById("quit-page-container").appendChild(infoPageContent);
+  infoPageContent.innerHTML =
+    '<div class="top-path-label grey-label">Contact.exe</div><div class="popup-content"><p>This programme is under development.</p><p>Meanwhile, you can contact me via</p><p><a href="http://www.linkedin.com/in/oleksiy-onyshchenko" target="_blank">www.linkedin.com/in/oleksiy-onyshchenko</a></p></div><br><div><span class="close-popup-btn" onclick="closeHelpPage()">[ Close this window ]</span><br><br></div>';
+
+  setSelectedFileName(activeLine); // shows file name on the right panel - bottom file name section
+}
+
+// this function shows a temporary "Info" message that Resume is not yet available
+function resumePdf() {
+  removeActiveLineStyle();
+
+  var activeLine = document.getElementById("resume-pdf");
+  activeLine.classList.add("active-line");
+  var rightContent = document.getElementById("right-content");
+  rightContent.innerHTML = "<p>Resume is not available yet..</p>";
 
   setSelectedFileName(activeLine); // shows file name on the right panel - bottom file name section
 }
@@ -68,43 +117,7 @@ function workExperienceInfo() {
   activeLine.classList.add("active-line");
   var rightContent = document.getElementById("right-content");
   rightContent.innerHTML =
-    '<div class="center-txt"><p>*******************</p><p>* Work Experience *</p><p>*******************</p></div><p class="yellow-txt">2015 - present:</p><p>Lead QA Analyst at Luxoft UK LTD (London)</p><p class="yellow-txt">2013 - 2015:</p><p>Senior QA Engineer at Luxoft Ukraine (Kyiv)</p><p class="yellow-txt">2012 - 2013:</p><p>QA Engineer at Samsung R&D Ukraine (Kyiv)</p><p class="yellow-txt">2009 - 2012:</p><p>Software Engineer at Orneon LTD (Vinnytsya)</p><p class="yellow-txt">2008 - 2009:</p><p>Financial adviser at UkrSibBank BNP Paribas (Vinnytsya)</p>';
-
-  setSelectedFileName(activeLine); // shows file name on the right panel - bottom file name section
-}
-
-// this function creates Web element to show an "Error" message when user tries to navigate to the "Root" folder
-function root() {
-  createBlankPage();
-
-  var errorPageContainer = document.createElement("div");
-  var errorPageContent = document.createElement("div");
-  errorPageContainer.id = "alert-page-container";
-  errorPageContent.id = "alert-page-content";
-  document.getElementById("blank-page").appendChild(errorPageContainer);
-  document.getElementById("alert-page-container").appendChild(errorPageContent);
-  errorPageContent.innerHTML =
-    '<div class="top-path-label red-label">Access Denied</div><div class="popup-content"><p>You don&#39;t have an access to the "Root" folder</p></div><div><br><span class="delete-yes-no-button" onclick="closeHelpPage()">[ Close this window ]</span><br><br></div>';
-}
-
-// this function creates Web element to show an "Info" message when user tries to run a contact.exe programme
-function contactExe() {
-  createBlankPage();
-  removeActiveLineStyle();
-
-  var activeLine = document.getElementById("contact-exe");
-  activeLine.classList.add("active-line");
-  var rightContent = document.getElementById("right-content");
-  rightContent.innerHTML =
-    "<p>Preview is not available for executable programme.</p>";
-  var infoPageContainer = document.createElement("div");
-  var infoPageContent = document.createElement("div");
-  infoPageContainer.id = "quit-page-container";
-  infoPageContent.id = "quit-page-content";
-  document.getElementById("blank-page").appendChild(infoPageContainer);
-  document.getElementById("quit-page-container").appendChild(infoPageContent);
-  infoPageContent.innerHTML =
-    '<div class="top-path-label grey-label">Contact.exe</div><div class="popup-content"><p>This programme is under development.</p><p>Meanwhile, you can contact me via</p><p><a href="http://www.linkedin.com/in/oleksiy-onyshchenko" target="_blank">www.linkedin.com/in/oleksiy-onyshchenko</a></p></div><br><div><span class="close-popup-btn" onclick="closeHelpPage()">[ Close this window ]</span><br><br></div>';
+    '<p class="center-txt">*******************<br>* Work Experience *<br>*******************</p><p><span class="yellow-txt">2015 - present:</span><br><span>Lead QA Analyst at Luxoft UK LTD (London)</span></p><p><span class="yellow-txt">2013 - 2015:</span><br><span>Senior QA Engineer at Luxoft Ukraine (Kyiv)</span></p><p><span class="yellow-txt">2012 - 2013:</span><br><span>QA Engineer at Samsung R&D Ukraine (Kyiv)</span></p><p><span class="yellow-txt">2009 - 2012:</span><br><span>Software Engineer at Orneon LTD (Vinnytsya)</span></p><p><span class="yellow-txt">2008 - 2009:</span><br><span>Financial adviser at UkrSibBank BNP Paribas (Vinnytsya)</span></p>';
 
   setSelectedFileName(activeLine); // shows file name on the right panel - bottom file name section
 }
